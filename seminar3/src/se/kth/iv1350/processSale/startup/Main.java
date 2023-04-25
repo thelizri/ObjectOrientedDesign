3680ed89c0990559c67da6375a24d436eb741c83
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-25 18:28:40 +0200

    update

--- seminar3/src/se/kth/iv1350/processSale/startup/Main.java
+++ seminar3/src/se/kth/iv1350/processSale/startup/Main.java
@@ -1,7 +1,16 @@
 package se.kth.iv1350.processSale.startup;
 
+import se.kth.iv1350.processSale.integration.*;
+import se.kth.iv1350.processSale.controller.Controller;
+import se.kth.iv1350.processSale.view.View;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DiscountDatabase disDb = new DiscountDatabase();
+        ExternalAccountingSystem accSys = new ExternalAccountingSystem();
+        ExternalInventorySystem invSys = new ExternalInventorySystem();
+        Printer printer = new Printer();
+        Controller controller = new Controller(invSys, accSys, printer, disDb);
+        View view = new View(controller);
     }
 }
--- seminar3/src/se/kth/iv1350/processSale/view/View.java
+++ seminar3/src/se/kth/iv1350/processSale/view/View.java
@@ -1,4 +1,7 @@
 package se.kth.iv1350.processSale.view;
 
+import se.kth.iv1350.processSale.controller.Controller;
 public class View {
+    public View(Controller controller) {
+    }
 }
