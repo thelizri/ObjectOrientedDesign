996f9b821c59a22b85aa7dbc4fcd38d252475630
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-04-29 23:28:54 +0200

    Docstring to Sale class

--- seminar3/src/se/kth/iv1350/processSale/model/Sale.java
+++ seminar3/src/se/kth/iv1350/processSale/model/Sale.java
@@ -15,8 +15,9 @@ public class Sale {
     private float amountPaid;
     private float change;
 
-    private String customerID;
-
+    /**
+     * Constructs a new instance of the Sale class with default values.
+     */
     public Sale() {
         this.dateTime = LocalDateTime.now();
         this.itemList = new ArrayList<>();
@@ -27,22 +28,42 @@ public class Sale {
         this.discount = 0;
     }
 
+    /**
+     * Gets the total price of the sale.
+     * @return The total price of the sale.
+     */
     public float getTotal() {
         return totalPrice;
     }
 
+    /**
+     * Gets the amount paid for the sale.
+     * @return The amount paid for the sale.
+     */
     public float getAmountPaid() {
         return amountPaid;
     }
 
+    /**
+     * Calculates and returns the remaining amount to be paid for the sale.
+     * @return The remaining amount to be paid for the sale.
+     */
     public float getRemainingAmount() {
         return totalPrice - discount - amountPaid;
     }
 
+    /**
+     * Gets a SaleDTO object representing the sale.
+     * @return A SaleDTO object representing the sale.
+     */
     public SaleDTO getSaleDTO() {
         return new SaleDTO(dateTime, itemList, totalPrice, totalVAT, amountPaid, change);
     }
 
+    /**
+     * Generates a receipt string for the sale.
+     * @return A receipt string for the sale.
+     */
     public String getReceipt() {
         StringBuilder receiptBuilder = new StringBuilder();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMM yyyy HH:mm");
@@ -66,7 +87,11 @@ public class Sale {
     }
 
 
-
+    /**
+     * Adds an item to the sale with the specified quantity.
+     * @param item The item to add to the sale.
+     * @param quantity The quantity of the item to add to the sale.
+     */
     public void addItem(Item item, int quantity) {
         item.setQuantity(quantity);
         itemList.add(item);
@@ -75,10 +100,18 @@ public class Sale {
         totalVAT += itemPrice * item.getRateVAT();
     }
 
+    /**
+     * Applies a discount to the sale.
+     * @param amount The amount of the discount to apply.
+     */
     public void applyDiscount(float amount) {
         this.discount = amount;
     }
 
+    /**
+     * Closes the sale, indicating that it is complete.
+     * @return true if the sale has been closed successfully, false otherwise.
+     */
     public boolean closeSale(){
         if (getRemainingAmount() > 0){
             return false;
@@ -86,12 +119,13 @@ public class Sale {
         return true;
     }
 
+
+    /**
+     * Adds a payment to the sale.
+     * @param amount The amount of the payment to add to the sale.
+     */
     public void pay(float amount) {
         amountPaid += amount;
-    }
-
-    public void setCustomerID(String customerID){
-        this.customerID = customerID;
     }
 }
 
