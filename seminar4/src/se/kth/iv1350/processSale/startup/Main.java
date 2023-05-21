f9d9a623f9114839b14596ed3a3bd87918716d81
thelizri <karl.william.tony.carlstedt@gmail.com>
2023-05-21 22:40:30 +0200

    update

--- seminar4/src/se/kth/iv1350/processSale/startup/Main.java
+++ seminar4/src/se/kth/iv1350/processSale/startup/Main.java
@@ -1,11 +1,9 @@
 package se.kth.iv1350.processSale.startup;
 
 import se.kth.iv1350.processSale.controller.Controller;
-import se.kth.iv1350.processSale.integration.DiscountDatabase;
-import se.kth.iv1350.processSale.integration.ExternalAccountingSystem;
-import se.kth.iv1350.processSale.integration.ExternalInventorySystem;
-import se.kth.iv1350.processSale.integration.Printer;
+import se.kth.iv1350.processSale.integration.*;
 import se.kth.iv1350.processSale.utils.ExceptionLogger;
+import se.kth.iv1350.processSale.utils.Money;
 import se.kth.iv1350.processSale.view.View;
 
 import java.util.logging.Level;
@@ -13,16 +11,20 @@ import java.util.logging.Level;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            DiscountDatabase disDb = new DiscountDatabase();
-            ExternalAccountingSystem accSys = new ExternalAccountingSystem();
-            ExternalInventorySystem invSys = new ExternalInventorySystem();
-            Printer printer = new Printer();
-            Controller controller = new Controller(invSys, accSys, printer, disDb);
-            View view = new View(controller);
-            view.run();
-        } catch (Exception exception) {
-            ExceptionLogger.logException(exception, Level.SEVERE, "An unknown error has disrupted the program.");
-        }
+
+//        try {
+//            DiscountDatabase disDb = new DiscountDatabase();
+//            ExternalAccountingSystem accSys = new ExternalAccountingSystem();
+//            ExternalInventorySystem invSys = new ExternalInventorySystem();
+//            Printer printer = new Printer();
+//            Controller controller = new Controller(invSys, accSys, printer, disDb);
+//            View view = new View(controller);
+//            view.run();
+//        } catch (Exception exception) {
+//            ExceptionLogger.logException(exception, Level.SEVERE, "An unknown error has disrupted the program.");
+//        }
+
+        var totalRevenueFileOutput = new TotalRevenueFileOutput();
+        totalRevenueFileOutput.update(new Money(200));
     }
 }
--- seminar4/src/se/kth/iv1350/processSale/view/TotalRevenueView.java
+++ seminar4/src/se/kth/iv1350/processSale/view/TotalRevenueView.java
@@ -3,17 +3,43 @@ package se.kth.iv1350.processSale.view;
 import se.kth.iv1350.processSale.utils.Money;
 import se.kth.iv1350.processSale.utils.Observer;
 
-//Subject and Observers
+/**
+ * This is a concrete Observer class called TotalRevenueView that displays the total revenue.
+ * It follows the Observer design pattern and updates the total revenue whenever a change occurs.
+ * The change is notified by the Subject.
+ *
+ * The TotalRevenueView class implements the Observer interface and overrides the update method.
+ */
 public class TotalRevenueView implements Observer {
+
+    private Money totalRevenue;
+
+    /**
+     * Constructs a new TotalRevenueView object with total revenue set to zero.
+     * Represents the total revenue as Money object.
+     * It is updated whenever the update method is called.
+     */
+    public TotalRevenueView() {
+        totalRevenue = new Money();
+    }
 
     /**
      * This method is called by the observed Subject when a change occurs.
-     * The Money object that is passed contains the updated state.
+     * The total revenue is updated with the Money object that is passed which contains the updated state.
      *
-     * @param money the updated state
+     * @param money the Money object containing the amount to be added to the total revenue
      */
     @Override
     public void update(Money money) {
+        totalRevenue = totalRevenue.add(money);
+    }
 
+    /**
+     * This method displays the total revenue to the standard output.
+     * The revenue is formatted as a floating-point number with two decimal places.
+     */
+    public void displayTotalRevenue() {
+        System.out.println(String.format("%-20s %10.2f kr\n", "Total Revenue: ", totalRevenue.getAmountFloat()));
     }
 }
+
